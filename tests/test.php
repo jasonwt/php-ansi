ea7217ec1071ad5819a4c2c4f7bf127849d7c4d5
Jason Thompson <jasonwt@gmail.com>
2022-11-13 10:56:24 -0800

    test.php updates

--- tests/test.php
+++ tests/test.php
@@ -8,11 +8,9 @@
 
     require_once(__DIR__ . "/../src/Ansi.php");
     require_once(__DIR__ . "/../src/RGB.php");
-    require_once(__DIR__ . "/../src/sequences/ColorSequence.php");
 
     use ansi\Ansi;
     use ansi\RGB;
-    use ansi\sequences\ColorSequence;
 
     echo Ansi::NEW()->
         SaveScreen()->
@@ -20,44 +18,46 @@
         MoveCursorDown(10);    
     
     for ($r = 0; $r < 256; $r+=8) {
-        for ($g = 0; $g < 256; $g+=4)
-            echo Ansi::TEXT(" ",null, RGB::NEW($r, $g, intval(($g+$r)/2)), 24);
+        for ($g = 0; $g < 256; $g+=4) {
+            $b = intval(($g+$r)/2);
+            echo Ansi::TEXT(" ",null, RGB::NEW($r, $g, $b), 24);
+        }
         
         echo Ansi::TEXT("\n");
     }
 
     echo Ansi::TEXT("\n")->
         AddText("TEXT\n\n")->
-        SetColors(RGB::HEX('ff0000'))->
-        AddText("\tSetColors(RGB::HEX('ff0000'))\n\n")->
-        SetForegroundColor(0, 255, 0)->
-        AddText("\t\tSetForegroundColor(0, 255, 0)\n\n")->
-        SetForegroundColor(0, 0, 255)->
-        AddText("\t\t\tSetForegroundColor(0, 0, 255)\n\n")->
-        SetColors(null,RGB::HEX('#ffffff'))->
-        AddText("\t\t\t\tSetColors(null,RGB::HEX('#ffffff'))\n\n")->        
-        SetColors(RGB::HEX('00ff00'),RGB::HEX('#0000ff'))->
-        AddText("\t\t\t\t\tSetColors(RGB::HEX('00ff00'),RGB::HEX('#0000ff'))\n\n")->        
-        CloseLast()->
-        AddText("\t\t\t\tCloseLast()\n\n")->
-        CloseLast()->
-        AddText("\t\t\tCloseLast()\n\n")->
-        Underline()->
-        AddText("\t\t\t\tUnderline()\n\n")->
-        CloseLast()->
-        AddText("\t\t\tCloseLast()\n\n")->
-        CloseLast()->
-        AddText("\t\tCloseLast()\n\n")->
-        Strike()->
-        AddText("\t\t\tStrike()\n\n")->
-        Blink()->
-        AddText("\t\t\t\tBlink()\n\n")->
-        CloseLast()->
-        AddText("\t\t\tCloseLast()\n\n")->
-        CloseLast()->
-        AddText("\t\tCloseLast()\n\n")->
-        CloseLast()->
-        AddText("\tCloseLast()\n\n")->
+        SetForegroundColor(255,0,0)->
+            AddText("\tSetForegroundColor(255,0,0)\n\n")->
+            SetColors(RGB::NEW(0,255,0))->
+                AddText("\t\tSetColors(RGB::NEW(0,255,0))\n\n")->
+                SetColors(RGB::NEW(0,0,255), null)->
+                    AddText("\t\t\tSetColors(RGB::NEW(0,0,255), null)\n\n")->
+                    SetBackgroundColor(255,255,255)->        
+                        AddText("\t\t\t\tSetBackgroundColor(255,255,255)\n\n")->
+                        SetColors(RGB::HEX('00ff00'),RGB::HEX('#0000ff'))->
+                            AddText("\t\t\t\t\tSetColors(RGB::HEX('00ff00'),RGB::HEX('#0000ff'))\n\n")->        
+                        CloseLast()->
+                        AddText("\t\t\t\tCloseLast()\n\n")->
+                    CloseLast()->
+                    AddText("\t\t\tCloseLast()\n\n")->
+                    Underline()->
+                        AddText("\t\t\t\tUnderline()\n\n")->
+                    CloseLast()->
+                    AddText("\t\t\tCloseLast()\n\n")->
+                CloseLast()->
+                AddText("\t\tCloseLast()\n\n")->
+                Strike()->
+                    AddText("\t\t\tStrike()\n\n")->
+                    Blink()->
+                        AddText("\t\t\t\tBlink()\n\n")->
+                    CloseLast()->
+                    AddText("\t\t\tCloseLast()\n\n")->
+                CloseLast()->
+                AddText("\t\tCloseLast()\n\n")->
+            CloseLast()->
+            AddText("\tCloseLast()\n\n")->
         CloseLast()->
         AddText("TEXT\n\n");
 
